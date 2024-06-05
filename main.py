339011140410b5cb47ae2b3acec8a2b52c3fecf6
Juliana C. Teixeira <julianact2004@gmail.com>
2024-06-05 18:54:00 -0300

    Sistema de Ranking Local

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, random, os, time
+from tkinter import simpledialog
 
 pygame.init()
 
@@ -36,14 +37,16 @@ branco = (255,255,255)
 preto = (0,0,0)
 vermelho = (255,0,0)
 
-def jogar():
+nome = simpledialog.askstring("Bem-Vindo à Penacony!", "Insira um username: ")
+
+def jogar(nome):
 
     pygame.mixer.music.load("assetsHSR/AgainstTheDay.mp3")
-    #pygame.mixer.music.play(-1) # Botando a musica pra tocar / -1 para tocar em loop
+    pygame.mixer.music.play(-1) # Botando a musica pra tocar / -1 para tocar em loop
     #pygame.mixer.Sound.play(misselSom) # Tocando o som do missel 1vez ja q ele ja começa caindo
 
     posicaoXpersona = 500
-    posicaoYpersona = 300
+    posicaoYpersona = 500
     movimentoXpersona = 0
     movimentoYpersona = 0
     larguraPersona = 150
@@ -61,7 +64,7 @@ def jogar():
     larguraMeme1 = 100
     velMeme1 = 10
 
-    dificuldade = 60
+    dificuldade = 50
     pontos = 0
 
     while True:
@@ -78,14 +81,14 @@ def jogar():
             elif evento.type == pygame.KEYUP and evento.key == pygame.K_LEFT:
                 movimentoXpersona = 0
             # Controle do personagem eixo Y
-            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
-                movimentoYpersona = - 10
-            elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
-                movimentoYpersona = + 10
-            elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
-                movimentoYpersona = 0
-            elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
-                movimentoYpersona = 0
+            #elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
+            #    movimentoYpersona = - 10
+            #elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_DOWN:
+            #    movimentoYpersona = + 10
+            #elif evento.type == pygame.KEYUP and evento.key == pygame.K_UP:
+            #    movimentoYpersona = 0
+            #elif evento.type == pygame.KEYUP and evento.key == pygame.K_DOWN:
+            #    movimentoYpersona = 0
         
         posicaoXpersona = posicaoXpersona + movimentoXpersona  
         posicaoYpersona = posicaoYpersona + movimentoYpersona
@@ -122,7 +125,7 @@ def jogar():
         tela.blit(memeAllSeer, (posicaoXmeme1, posicaoYmeme1))
         
         # Mostra aos pontos
-        texto = fonte.render("Pontos:" + str(pontos), True, branco)
+        texto = fonte.render(nome + " pontos:" + str(pontos), True, branco)
         tela.blit(texto,(10,10))
         # -------------------------------------------------------
 
@@ -138,13 +141,13 @@ def jogar():
         #print( len( list( set(pixelMisselY).intersection(set(pixelsPersonaY) ) ) ) )
         if len( list( set( pixelBirdY).intersection (set(pixelsPersonaY)))) > dificuldade:
             if len ( list ( set(pixelBirdX).intersection(set(pixelsPersonaX) ) ) ) > dificuldade:
-                dead()
+                dead(nome, pontos)
             #else:
                 #print("Ainda vivo, mas por pouco!")
 
         elif len( list( set( pixelMeme1Y ).intersection (set( pixelsPersonaY )) )) > dificuldade:
             if len (list ( set( pixelMeme1X ).intersection(set( pixelsPersonaX )) )) > dificuldade:
-                dead()
+                dead(nome, pontos)
         #else:
             #print("Ainda vivo")
 
@@ -154,9 +157,24 @@ def jogar():
         pygame.display.update()
         relogio.tick(60)
 
-def dead():
+def dead(nome, pontos):
     pygame.mixer.music.stop()
     pygame.mixer.Sound.play(somStelleMorte)
+
+    jogadas = {}
+    try:
+        arquivo = open("historico.txt","r", encoding="utf-8")
+        jogadas = eval(arquivo.read())
+        arquivo.close()
+    except:
+        arquivo = open("historico.txt","w", encoding="utf-8")
+        arquivo.close()
+
+    jogadas[nome] = pontos
+    arquivo = open("historico.txt","w", encoding="utf-8")
+    arquivo.write(str(jogadas))
+    arquivo.close()  
+    
     while True:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
@@ -164,25 +182,25 @@ def dead():
                 elif evento.type == pygame.MOUSEBUTTONDOWN:
                     #print(evento.pos)
                     if botaoJogar.collidepoint(evento.pos):
-                        jogar()
+                        jogar(nome)
                 elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_RETURN:
-                    jogar()
+                    jogar(nome)
             
             tela.fill(preto)
             textoTitulo = fonteTitulo.render("Você Morreu :(", True, branco)
             tela.blit(textoTitulo,(100,250))
-            botaoJogar = pygame.draw.rect(tela, branco, (100,400,400,50))
+            botaoJogar = pygame.draw.rect(tela, branco, (100,400,400,50),0,20)
             textoJogar = fonte.render("Tentar Novamente", True, preto)            
-            tela.blit(textoJogar,(125,390))
+            tela.blit(textoJogar,(125,395))
             textoEnter = fonteMiuda.render("ENTER para tentar novamente", True, branco)
-            tela.blit(textoEnter, (100,450))
+            tela.blit(textoEnter, (110,450))
             tela.blit(stelleMorreu, (600,150))
             
             pygame.display.update()
             relogio.tick(60)
 
 def menu():
-    #pygame.mixer.music.play(-1)
+    pygame.mixer.music.play(-1)
     
     while True:            
             for evento in pygame.event.get():
@@ -191,17 +209,58 @@ def menu():
                 elif evento.type == pygame.MOUSEBUTTONDOWN:
                     #print(evento.pos)
                     if botaoJogar.collidepoint(evento.pos):
-                        jogar()
+                        jogar(nome)
+                    elif botaoRank.collidepoint(evento.pos):
+                        rank()
 
             tela.fill(branco)
             tela.blit(fundoMenu,(0,0))
             textoTitulo = fonteTitulo.render("Bem Vindo à Penacony!", True, branco)
-            tela.blit(textoTitulo,(50,250))
-            botaoJogar = pygame.draw.rect(tela, branco, (100,400,400,50))
+            tela.blit(textoTitulo,(90,250))
+            botaoJogar = pygame.draw.rect(tela, branco, (100,400,400,50),0,20)
             textoJogar = fonte.render("Jogar", True, preto)
             tela.blit(textoJogar,(250,390))
-            
+            botaoRank = pygame.draw.rect(tela, branco,(7,615,170,50),0,20)
+            textoRank = fonte.render("Ranking", True, preto)
+            tela.blit(textoRank,(20,605))
             
             pygame.display.update()
             relogio.tick(60)
+
+def rank():
+    estrelas = {}
+    try:
+        arquivo = open("historico.txt","r",encoding="utf-8" )
+        estrelas = eval(arquivo.read())
+        arquivo.close()
+    except:
+        pass
+    
+    nomes = sorted(estrelas, key=estrelas.get,reverse=True)
+    print(estrelas)
+    
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                quit()
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                if botaoVoltar.collidepoint(evento.pos):
+                    menu()
+
+        tela.fill(preto)        
+        botaoVoltar = pygame.draw.rect(tela, branco, (7,7,130,50),0,20)
+        textoVoltar = fonte.render("Voltar", True, preto)
+        tela.blit(textoVoltar,(10,10))
+        
+        posicaoY = 50
+        for key,nome in enumerate(nomes):
+            if key == 13:
+                break
+            textoJogador = fonte.render(nome + " - "+str(estrelas[nome]), True, branco)
+            tela.blit(textoJogador, (300,posicaoY))
+            posicaoY = posicaoY + 40
+
+        pygame.display.update()
+        relogio.tick(60)
+  
 menu()
